3cd37614cf2da4cc1156a92d5058ee30f618712a
ljx269919845 <ljx269919845@qq.com>
2018-12-07 18:04:22 +0800

    tmp

--- src/data/api.js
+++ src/data/api.js
@@ -11,7 +11,7 @@
 class Request{
     constructor(){
         this.failedFunc = ()=>{};
-        this.host = "http://127.0.0.1:8087"
+        this.host = "http://172.16.7.174:8087"
     }
 
     post(url, urlParam, queryParam, bodyParam){
@@ -152,7 +152,7 @@ class AppointItem {
     this.total += appointSet.userNum;
     if (appointSet.doctor.status === 1) {
       this.realyTotal += appointSet.userNum;
-      this.totalSurplus += (appointSet.userNum - appointSet.surplusNum);
+      this.totalSurplus += appointSet.surplusNum;
     }
     this.appointSets.push(appointSet);
   }
